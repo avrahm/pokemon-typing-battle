35db00ebf5b35e2145b1008eceab9f7bed0418b2
Avrahm <avrahmk@me.com>
2020-10-03 21:51:17 -0400

    skill mode

--- assets/js/index.js
+++ assets/js/index.js
@@ -8,15 +8,32 @@ let isPlaying = false;
 let score = 0;
 
 let frames = 0;
+let letters = '';
 
-const homeRowLetters = "asdfghjkl;'";
+function setSkillMode(skill) {
 
-var keyArray = homeRowLetters.split('');
+    switch (skill) {
+        case "home":
+            letters = "ASDFGHJKL;'";
+            break;
+        case "top":
+            letters = "QWERTYUIOP"; //shift letters for hard mode :"
+            break;
+        case "bottom":
+            letters = "ZXCVBNM,./"; //shift letters for hard mode <>?
+            break;
+    }
+
+    return keyArray = letters.split('');
+}
 
 const draw = (obj, type) => {
-    if (type === "tile2") {
+    if (type === "tile1") {
         ctx.fillStyle = "black";
-        ctx.fillRect(obj.x, obj.y, obj.width, obj.height)
+        ctx.fillRect(obj.x, obj.y, obj.width, obj.height);
+        ctx.font = '80px Sans-serif';
+        ctx.fillStyle = 'white';
+        ctx.fillText(obj.key, obj.x, obj.y);
     }
     if (type === "tile") {
         ctx.font = '80px Sans-serif';
@@ -45,7 +62,7 @@ class Tile {
 
     //hard
     // moveTile(direction, value) {
-    //     this[direction] += value * difficultySpeed;
+    //     this[direction] += value; //* difficultySpeed;
     // }
 }
 
@@ -58,32 +75,40 @@ class Game {
 
     spawnTile() {
         let rKey = keyArray[Math.floor(Math.random() * keyArray.length)];
-        let rX = Math.floor(Math.random() * (ctx.width - 80)) + 65;
-        let rY = Math.floor(Math.random() * (ctx.height - 80)) + 25;
-        // let rWidth = 45;
-        // let rHeight = 45;
+        let rX = Math.floor(Math.random() * (ctx.width));
+        let rY = Math.floor(Math.random() * (ctx.height));
+        let rWidth = 85;
+        let rHeight = 85;
+        if (rX + rWidth > ctx.width) {
+            rX -= 50
+        }
+        if (rX - rWidth < 0) {
+            rX += 50
+        }
+        if (rY + rHeight > ctx.height) {
+            rY -= 50
+        }
+        if (rY - rHeight < 0) {
+            rY += 50
+        }
 
-        let newTile = new Tile(rKey, rX, rY);
+        let newTile = new Tile(rKey, rX, rY, rWidth, rHeight);
         this.tileArray.push(newTile);
     }
 }
 
 document.onkeydown = (e) => {
-    // console.log(theGame.tileArray)
     theGame.tileArray.map((tile, i) => {
-        if (e.key == tile.key) {
-            // console.log(key,i)
+        if (e.key.toUpperCase() == tile.key) {
             score += 1;
             document.getElementById("score").innerHTML = score;
-            theGame.tileArray.splice(i,1);
+            theGame.tileArray.splice(i, 1);
+        } else if (e.key.toUpperCase() !== tile.key) {
+            score -= 1;
+            document.getElementById("score").innerHTML = score;
         }
     })
 }
-
-
-
-
-
 
 function mainLoop() {
     frames++;
@@ -92,8 +117,9 @@ function mainLoop() {
         draw(tile, "tile")
     })
 
-    if (frames % 150 === 0 && theGame.tileArray.length < 1) theGame.spawnTile();
-    console.log(theGame.tileArray.length)
+    if (frames % 150 === 0
+        && theGame.tileArray.length < 1
+    ) theGame.spawnTile();
     if (isPlaying === true) requestId = requestAnimationFrame(mainLoop);
 }
 
@@ -106,7 +132,8 @@ function stopGame() {
 }
 
 
-startGame = () => {
+startGame = (skill) => {
+    setSkillMode(skill);
     isPlaying = true;
     theGame = new Game()
     mainLoop();
